ee678575345c0b23b01f15d493fdfd90d3ca4d9c
Pedro Morais <pedro.s.morais@gmail.com>
2021-09-11 21:58:39 -0300

    first mutation

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,3 +1,4 @@
+// QUERIES
 const post = async (_, { id }, { dataSources }) => {
   const post = dataSources.postApi.getPost(id);
   return post;
@@ -8,11 +9,26 @@ const posts = async (_, { input }, { dataSources }) => {
   return posts;
 };
 
+// MUTATIONS
+const createPost = async (_, args, { dataSources }) => {
+  console.log('ARGS', args);
+  return {
+    id: '860',
+    title: 'Et voluptatem nulla omnis et iusto ullam.',
+    body: 'Ad non pariatur..',
+    userId: '29',
+    indexRef: 10,
+    createdAt: '2018-08-10T23:41:51.714Z',
+  };
+};
+
+// FIELD RESOLVERS
 const user = async ({ userId }, _, { dataSources }) => {
   return dataSources.userApi.batchLoadById(userId);
 };
 
 export const postResolvers = {
   Query: { post, posts },
+  Mutation: { createPost },
   Post: { user },
 };
